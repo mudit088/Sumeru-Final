08cb7e31a555b562cb0087507c90f60992a07768
mudit088 <muditsri25@gmail.com>
2024-08-28 03:14:37 +0530

    updated

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    './index.html',
+    './Services.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
